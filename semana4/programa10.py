d7dc1e44cb470318a1b52b86bcc5e9d8aac765a4
JOS� ALONSO DOMÍNGUEZ CASTILLO <122809477+Alonso-Dominguez@users.noreply.github.com>
2023-02-07 20:59:03 +0000

    Crear una aplicación que lea 2 números enteros, los compare y muestre el numero mayo, en caso de que sean iguales muestre None

---
+++ semana4/programa10.py
@@ -0,0 +1,56 @@
+""""
+Nombre: José Alonso Domínguez Castillo
+Fecha: 07/02/23
+Descripción
+"""
+n1=int(input("Número 1: "))
+n2=int(input("Número 2: "))
+if n1 > n2:
+    print(n1)
+if n2 > n1:
+    print(n2)
+if n1 == n2:
+    print(None)
+if n2 > n1:
+    print(n2)
+if n1 > n2:
+    print(n1)
+else:
+    print(None)
+if n1 > n2:
+    print(n1)
+elif n2 > n1:
+    print(n2)
+else:
+    print(None)
+if n1 < n2:
+    print(n2)
+if n2 < n1:
+    print(n1)
+if n1 == n2:
+    print(None)
+if n2 < n1:
+    print(n1)
+if n1 < n2:
+    print(n2)
+else:
+    print(None)
+if n2 > n1:
+    print(n2)
+if n2 < n1:
+    print(n1)
+else:
+    print(None)
+if n2 < n1 > n2:
+    print(n1)
+elif n1 < n2 > n1:
+    print(n2)
+else:
+    print(None)
+if n1 <= n2:
+    if n1 == n2:
+        print(None)
+    else:
+        print(n2)
+else:
+    print(n1)
